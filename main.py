20c7692d158ded6e2dfc37c851b970f751ef67d7
YevrahDev <109664103+YevrahDev@users.noreply.github.com>
2022-08-02 23:09:08 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get('https://ph.indeed.com/jobs?q=python%20data%20engineer&fromage=1&vjk=c034bf4d2b573e1c').text
+soup = BeautifulSoup(html_text, 'lxml')
+job_list = soup.find('ul', class_='jobsearch-ResultsList css-0')
+jobs = job_list.find_all('td', class_='resultContent')
+
+for job in jobs:
+    job_title = job.find('h2').text
+
+    company_name = job.find('span', class_='companyName').text
+    try:
+        salary = job.find('div', class_='attribute_snippet').text
+        if salary.split()[3] == "month":
+            dir_salary = salary.split()[1]
+            print('')
+            print(f"Job Title: {job_title.strip()}")
+            print(f"Company Name: {company_name.strip()}")
+            print(f"Salary: {dir_salary.strip()}")
+
+        else:
+            min_salary = salary.split()[1]
+            max_salary = salary.split()[4]
+            print('')
+            print(f"Job Title: {job_title.strip()}")
+            print(f"Company Name: {company_name.strip()}")
+            print(f"Salary: {min_salary.strip()} to {max_salary.strip()}")
+
+    except:
+        dir_salary = "no salary"
+        print('')
+        print(f"Job Title: {job_title.strip()}")
+        print(f"Company Name: {company_name.strip()}")
+        print(f"Salary: {dir_salary.strip()}")
+

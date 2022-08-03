3bbcd5e5ae6d6e2ffece6b2b05dfab0b140c8779
YevrahDev <109664103+YevrahDev@users.noreply.github.com>
2022-08-03 22:56:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 from csv import writer
 
-job_to_search = input("Input Job Title to search: ")
-key_job_title = input("Input a key Job Title: ")
-
 def urlGenerator ():
     url_for_search = 'https://ph.indeed.com/jobs?q='
     word_counter = 0
@@ -17,23 +14,18 @@ def urlGenerator ():
         else:
             url_for_search += '+'
     return url_for_search
-urlGenerator()
 
-html_text = requests.get(urlGenerator()).text
-soup = BeautifulSoup(html_text, 'lxml')
-
-try:
-    max_page = soup.find('div', id='searchCountPages').text
-    print(f'{max_page.split()[3]} max pages')
-except:
-    print("Try again or enter a number lower than 10 for safety.")
-
-def searchStart(urlGet):
-    html_text = requests.get(urlGet).text
+def csv_maker():
     with open('JobsSalary.csv', 'a+', encoding='utf8', newline='') as f:
         the_writer = writer(f)
         header = ['Job Title', 'Company Name', 'Fixed Salary', 'Min Salary', 'Max Salary']
         the_writer.writerow(header)
+
+def searchStart(urlGet):
+    html_text = requests.get(urlGet).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    with open('JobsSalary.csv', 'a+', encoding='utf8', newline='') as f:
+        the_writer = writer(f)
         job_list = soup.find('ul', class_='jobsearch-ResultsList css-0')
         jobs = job_list.find_all('td', class_='resultContent')
         for job in jobs:
@@ -64,14 +56,29 @@ def urlWithPages(pages):
     urlGenerator()
     return urlGenerator() + 'start=' + str((pages - 1) * 10)
 
+job_to_search = input("Input Job Title to search: ")
+key_job_title = input("Input a key Job Title: ")
+urlGenerator()
+html_text = requests.get(urlGenerator()).text
+soup = BeautifulSoup(html_text, 'lxml')
+try:
+    max_page = soup.find('div', id='searchCountPages').text
+    print(f'{max_page.split()[3]} max pages')
+except:
+    print("Try again or enter a number lower than 10 for safety.")
+
 max_page_by_user = int(input("Insert how many pages you want to extract (more pages, bigger data): "))
-if max_page_by_user <= 1:
-    urlGenerator()
-    searchStart(urlGenerator())
 
-else:
-    for x in range(1,max_page_by_user+1):
-        urlWithPages(x)
-        searchStart(urlWithPages(x))
-
-
+try:
+    if max_page_by_user <= 1:
+        urlGenerator()
+        csv_maker()
+        searchStart(urlGenerator())
+    else:
+        csv_maker()
+        for x in range(1,max_page_by_user+1):
+            urlWithPages(x)
+            searchStart(urlWithPages(x))
+    print("Data Generation Success")
+except:
+    print("No results from given keywords")

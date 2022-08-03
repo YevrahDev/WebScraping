ea30838870d03bd5bcf91ab8431ac95368b93c1a
YevrahDev <109664103+YevrahDev@users.noreply.github.com>
2022-08-03 22:34:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,77 @@
 from bs4 import BeautifulSoup
 import requests
+from csv import writer
 
-html_text = requests.get('https://ph.indeed.com/jobs?q=python%20data%20engineer&fromage=1&vjk=c034bf4d2b573e1c').text
-soup = BeautifulSoup(html_text, 'lxml')
-job_list = soup.find('ul', class_='jobsearch-ResultsList css-0')
-jobs = job_list.find_all('td', class_='resultContent')
+job_to_search = input("Input Job Title to search: ")
+key_job_title = input("Input a key Job Title: ")
 
-for job in jobs:
-    job_title = job.find('h2').text
-
-    company_name = job.find('span', class_='companyName').text
-    print('')
-    print(f"Job Title: {job_title.strip()}")
-    print(f"Company Name: {company_name.strip()}")
-    try:
-        salary = job.find('div', class_='attribute_snippet').text
-        if salary.split()[3] == "month":
-            dir_salary = salary.split()[1]
-            print(f"Salary: {dir_salary.strip()}")
-
+def urlGenerator ():
+    url_for_search = 'https://ph.indeed.com/jobs?q='
+    word_counter = 0
+    search_word_count = len(job_to_search.split())
+    for url_bits in job_to_search.split():
+        url_for_search += url_bits
+        word_counter += 1
+        if word_counter == search_word_count:
+            url_for_search += '&'
         else:
-            min_salary = salary.split()[1]
-            max_salary = salary.split()[4]
-            print(f"Salary: {min_salary.strip()} to {max_salary.strip()}")
+            url_for_search += '+'
+    return url_for_search
+urlGenerator()
 
-    except:
-        dir_salary = "no salary"
-        print(f"Salary: {dir_salary.strip()}")
+html_text = requests.get(urlGenerator()).text
+soup = BeautifulSoup(html_text, 'lxml')
+
+try:
+    max_page = soup.find('div', id='searchCountPages').text
+    print(f'{max_page.split()[3]} max pages')
+except:
+    print("Try again or enter a number lower than 10 for safety.")
+
+def searchStart(urlGet):
+    html_text = requests.get(urlGet).text
+    with open('JobsSalary.csv', 'a+', encoding='utf8', newline='') as f:
+        the_writer = writer(f)
+        header = ['Job Title', 'Company Name', 'Fixed Salary', 'Min Salary', 'Max Salary']
+        the_writer.writerow(header)
+        job_list = soup.find('ul', class_='jobsearch-ResultsList css-0')
+        jobs = job_list.find_all('td', class_='resultContent')
+        for job in jobs:
+            job_title = job.find('h2').text
+            company_name = job.find('span', class_='companyName').text
+            if key_job_title.upper() in job_title.upper():
+                try:
+                    salary = job.find('div', class_='attribute_snippet').text
+                    if salary.split()[3] == "month":
+                        dir_salary = salary.split()[1]
+                        extracted_data = [job_title, company_name, dir_salary, "N/A", "N/A"]
+                        the_writer.writerow(extracted_data)
+
+                    else:
+                        min_salary = salary.split()[1]
+                        max_salary = salary.split()[4]
+                        extracted_data = [job_title, company_name, "N/A", min_salary, max_salary]
+                        the_writer.writerow(extracted_data)
+                except:
+                    dir_salary = "no salary"
+                    extracted_data = [job_title, company_name, dir_salary, "N/A", "N/A"]
+                    the_writer.writerow(extracted_data)
+            else:
+                extracted_data = [f'Not {key_job_title} related', "N/A", "N/A", "N/A", "N/A"]
+                the_writer.writerow(extracted_data)
+
+def urlWithPages(pages):
+    urlGenerator()
+    return urlGenerator() + 'start=' + str((pages - 1) * 10)
+
+max_page_by_user = int(input("Insert how many pages you want to extract (more pages, bigger data): "))
+if max_page_by_user <= 1:
+    urlGenerator()
+    searchStart(urlGenerator())
+
+else:
+    for x in range(1,max_page_by_user+1):
+        urlWithPages(x)
+        searchStart(urlWithPages(x))
+
 
